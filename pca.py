b21d564b813ac281b1ee0331b6deef3e7d6f4b2d
skozarekar <kozareks@umich.edu>
2020-04-04 13:17:45 -0400

    updated pca

--- pca.py
+++ pca.py
@@ -15,40 +15,41 @@ x = DF.iloc[:, list(range(2, 58))]
 y = DF.iloc[:, 1]
 X = StandardScaler().fit_transform(x)
 
-linear = KernelPCA(n_components=2, kernel="linear")
+linear = KernelPCA(n_components=10, kernel="linear", random_state=42)
 rbf = KernelPCA(n_components=2, kernel="rbf", gamma=0.008)
 sigmoid = KernelPCA(n_components=2, kernel="sigmoid", gamma=100)
 
+'''
 plt.figure(figsize=(11, 3))
 for subplot, pca, title in ((133, sigmoid, "Sigmoid kernel"), (132, rbf, "RBF kernel"), (131, linear, "Linear kernel")):
     X_reduced = pca.fit_transform(X)
+    
     plt.subplot(subplot)
     plt.title(title, fontsize=14)
     plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=y, cmap=plt.cm.hsv)
-    plt.xlabel("$z_1$", fontsize=18)
+    plt.xlabel("$PC_1$", fontsize=18)
     if subplot == 131:
-        plt.ylabel("$z_2$", fontsize=18, rotation=0)
-    #plt.grid(True)
+        plt.ylabel("$PC_2$", fontsize=18, rotation=0)
+    
     
 plt.show()
+'''
 
 from sklearn.svm import LinearSVC, NuSVC, SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 # trained on linear kernel
-X_train, X_test, y_train, y_test = train_test_split(X_reduced, y, test_size=0.20)
+X_reduced = linear.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X_reduced, y, test_size=0.20, random_state=42)
 
 
 #clf = LinearSVC(random_state=42, tol=1e-3, max_iter=100000000)
 #clf = NuSVC(nu = 0.9999999)
-clf = SVC(kernel='poly', degree=3, gamma='auto')
+clf = SVC(kernel='poly', degree=2, gamma='auto')
 
 clf.fit(X_train, y_train)
  
 preds = clf.predict(X_test)
 print(accuracy_score(y_test, preds))
                                                     
-
-
-
