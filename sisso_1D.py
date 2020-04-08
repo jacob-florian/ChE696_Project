cc81524e1726c94ff1cf2a8b3ac5787734236aa0
Jacob Florian <florianj@Jacobs-MacBook-Pro.local>
2020-04-08 18:51:06 -0500

    updated sisso features

--- sisso_1D.py
+++ sisso_1D.py
@@ -8,20 +8,29 @@ DF = pd.read_csv('AB_data.csv', index_col=0)
 DF = DF.loc[:, ~DF.columns.str.contains('^Unnamed')]
 DF = DF.sort_values(by='Class')
 
-print(DF.tail(20))
+DF['sisso1'] = abs((DF['Allred-Rochow EN']+DF['Atomic radius ratio'])-(DF['Allred-Rochow EN']*DF['Martynov-Batsanov Mean EN']))
+DF['sisso2'] = abs((DF['Allred-Rochow Mean EN']*DF['Zungerradius sum ratio']) - (DF['Martynov Ionic Character']/DF['Atomic radius ratio']))
 
-DF['sisso1'] = np.exp(DF['Family number sum']) * (DF['Allred-Rochow EN'] / DF['Martynov Ionic Character'])
-DF['sisso2'] = np.power(DF['Family number sum'], 3) * (DF['Allred-Rochow Mean EN'] / DF['Martynov Ionic Character'])
+DF['binary1'] = (DF['Allred-Rochow EN']*DF['Ionic radius ratio']) * (DF['Martynov-Batsanov Mean EN'] - DF['Ionic radius ratio'])
+DF['binary2'] = (DF['Allred-Rochow EN']*DF['Group number sum']) * (DF['Atomic radius sum'] - DF['Crystal radius sum'])
 
+'''
+less = DF.index[DF['binary1'] < 0.5].tolist()
+
+for i in less:
+	if DF.loc[i, 'Class'] == 1:
+		print(DF.loc[i, 'compound'])
+
+'''
 plt.figure(figsize=(10, 10))
-for i in range(1, 8):
+for i in [1, 2, 3, 4, 5, 6, 7]:
 	index = DF.index[DF['Class'] == i].tolist()
 	x = DF.loc[index, 'sisso1']
 	y = DF.loc[index, 'sisso2']
-	plt.scatter(x, y, s=5)
+	plt.scatter(x, y, s=10)
 
 plt.legend(['Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5', 'Class 6', 'Class 7'])
-plt.xlabel('exp(Family Number Sum) * (Allred-Rochow EN/Martynov Ionic Character)')
-plt.ylabel('(Family Number Sum)^3 * (Allred-Rochow EN/Martynov Ionic Character)')
+plt.xlabel('X1')
+plt.ylabel('X2')
 plt.show()
 
