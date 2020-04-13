89deb41272c69c1d414751b05b99ce9577ddc07c
skozarekar <kozareks@umich.edu>
2020-04-13 18:35:40 -0400

    updated pca w sensitivity analysis

--- pca.py
+++ pca.py
@@ -1,9 +1,13 @@
-# 2-component PCA
+# Kernel PCA
 
 from sklearn.decomposition import KernelPCA
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
 
 DF = pd.read_csv('AB_data.csv', index_col=0)
 DF = DF.loc[:, ~DF.columns.str.contains('^Unnamed')]
@@ -15,41 +19,39 @@ x = DF.iloc[:, list(range(2, 58))]
 y = DF.iloc[:, 1]
 X = StandardScaler().fit_transform(x)
 
-linear = KernelPCA(n_components=10, kernel="linear", random_state=42)
-rbf = KernelPCA(n_components=2, kernel="rbf", gamma=0.008)
-sigmoid = KernelPCA(n_components=2, kernel="sigmoid", gamma=100)
+components = np.arange(2,15,1)
+test_scores = []
+train_scores = []
 
-'''
-plt.figure(figsize=(11, 3))
-for subplot, pca, title in ((133, sigmoid, "Sigmoid kernel"), (132, rbf, "RBF kernel"), (131, linear, "Linear kernel")):
+for n in components:
+    pca = KernelPCA(n_components=n, kernel="linear")
     X_reduced = pca.fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(X_reduced, y, test_size=0.20, random_state=42, stratify=y)
     
-    plt.subplot(subplot)
-    plt.title(title, fontsize=14)
-    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=y, cmap=plt.cm.hsv)
-    plt.xlabel("$PC_1$", fontsize=18)
-    if subplot == 131:
-        plt.ylabel("$PC_2$", fontsize=18, rotation=0)
+    clf = SVC(kernel='rbf', gamma='auto')
+    clf.fit(X_train, y_train)
+    
+    train_preds = clf.predict(X_train)
+    train_scores.append(accuracy_score(y_train, train_preds))
+    
+    test_preds = clf.predict(X_test)
+    test_scores.append(accuracy_score(y_test, test_preds))
+    
+
+fig, ax = plt.subplots()
+ax.plot(components, test_scores, '-k', label="Validation")
+ax.plot(components, train_scores, '-r', label="Training")
+ax.set(xlabel='Number of Components', ylabel='Accuracy Score',
+       title='PCA Analysis')
+ax.legend(loc='lower right',fontsize='medium')
+
+
     
     
-plt.show()
-'''
 
-from sklearn.svm import LinearSVC, NuSVC, SVC
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-
-# trained on linear kernel
-X_reduced = linear.fit_transform(X)
-X_train, X_test, y_train, y_test = train_test_split(X_reduced, y, test_size=0.20, random_state=42)
-
-
-#clf = LinearSVC(random_state=42, tol=1e-3, max_iter=100000000)
-#clf = NuSVC(nu = 0.9999999)
-clf = SVC(kernel='poly', degree=2, gamma='auto')
-
-clf.fit(X_train, y_train)
- 
-preds = clf.predict(X_test)
-print(accuracy_score(y_test, preds))
+    
+    
+    
+    
+    
                                                     
