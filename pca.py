033ed7f0e538a4306865feffe4bddc2977289777
skozarekar <51425099+skozarekar@users.noreply.github.com>
2020-04-18 19:44:00 -0400

    final pca and svm scripts

--- pca.py
+++ pca.py
@@ -1,13 +1,14 @@
 # Kernel PCA
 
-from sklearn.decomposition import KernelPCA
-import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 import numpy as np
+import pandas as pd
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import KernelPCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 DF = pd.read_csv('AB_data.csv', index_col=0)
 DF = DF.loc[:, ~DF.columns.str.contains('^Unnamed')]
@@ -19,6 +20,7 @@ x = DF.iloc[:, list(range(2, 58))]
 y = DF.iloc[:, 1]
 X = StandardScaler().fit_transform(x)
 
+### PCA ANALYSIS FOR DIFFERENT N-COMPONENTS ### 
 components = np.arange(2,16,1)
 test_scores = []
 train_scores = []
@@ -37,29 +39,30 @@ for n in components:
     test_preds = clf.predict(X_test)
     test_scores.append(accuracy_score(y_test, test_preds))
     
-
-fig, ax = plt.subplots(figsize=(10,8))
+# plot accuracy vs n-components 
+fig, ax = plt.subplots(figsize=(6,4))
 ax.plot(components, test_scores, label="Validation")
 ax.plot(components, train_scores, label="Training")
 ax.set(xlabel='Number of Components', ylabel='Accuracy Score',
        title='PCA Analysis')
 ax.legend(loc='lower right',fontsize='x-large')
-#fig.savefig("pca_analysis.pdf")
+#fig.savefig("pca_analysis.pdf", dpi = 300)
 
 
+
+### PLOTTING 2 PCs ###
 pca = KernelPCA(n_components=2, kernel='linear')
 X_reduced = pca.fit_transform(X)
 
-### PLOTTING 
 DF['pca1'] = X_reduced[:, 0]
 DF['pca2'] = X_reduced[:, 1]
 
-plt.figure(figsize=(10, 10), dpi=100)
+plt.figure(figsize=(10, 10), dpi=400)
 for i in [1, 2, 3, 4, 5, 6, 7]:
 	index = DF.index[DF['Class'] == i].tolist()
 	x = DF.loc[index, 'pca1']
 	y = DF.loc[index, 'pca2']
-	plt.scatter(x, y, s=20)
+	plt.scatter(x, y, s=40)
 
 #plt.legend(['CsCl', 'NaCl', 'ZnS', 'CuAu', 'TlI', 'FeB', 'NiAs'], loc='lower right', fontsize='x-large')
 plt.xlabel("$PC_1$", fontsize=24)
@@ -67,6 +70,29 @@ plt.ylabel("$PC_2$", fontsize=24, rotation=0)
 plt.xticks(fontsize=18)
 plt.yticks(fontsize=18)
 plt.tight_layout()
-plt.savefig("pca.pdf")
+#plt.savefig("two_pcs.png")
 
 
+
+### PLOTTING 2 LDs ###
+y = DF['Class'].to_numpy()
+lda = LDA(n_components=2)
+X_lda = StandardScaler().fit_transform(lda.fit(X, y).transform(X))
+
+DF['lda1'] = X_lda[:, 0]
+DF['lda2'] = X_lda[:, 1]
+
+plt.figure(figsize=(10, 10), dpi=400)
+for i in [1, 2, 3, 4, 5, 6, 7]:
+	index = DF.index[DF['Class'] == i].tolist()
+	x = DF.loc[index, 'lda1']
+	y = DF.loc[index, 'lda2']
+	plt.scatter(x, y, s=40)
+
+#plt.legend(['CsCl', 'NaCl', 'ZnS', 'CuAu', 'TlI', 'FeB', 'NiAs'], loc='lower right', fontsize='x-large')
+plt.xlabel("$LD_1$", fontsize=24)
+plt.ylabel("$LD_2$", fontsize=24, rotation=0)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.tight_layout()
+#plt.savefig("two_lds.png")
--- svm_models.py
+++ svm_models.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-@author: Shivani Kozarekar
-"""
+#  Script to assess SVM models trained on data
 
-from sklearn.decomposition import KernelPCA
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
 import numpy as np
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.model_selection import GridSearchCV, train_test_split
+import pandas as pd
+from sklearn.decomposition import KernelPCA
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
 
 DF = pd.read_csv('AB_data.csv', index_col=0)
 DF = DF.loc[:, ~DF.columns.str.contains('^Unnamed')]
@@ -26,17 +25,17 @@ X_sisso = StandardScaler().fit_transform(np.transpose(np.vstack((sisso1, sisso2)
 ### PCA COMPONENTS ###
 x = DF.iloc[:, list(range(2, 58))]
 X = StandardScaler().fit_transform(x)
-linear = KernelPCA(n_components=15, kernel="linear", random_state=42)
+linear = KernelPCA(n_components=10, kernel="linear", random_state=42)
 X_pca = StandardScaler().fit_transform(linear.fit_transform(X))
 
 ### LDA COMPONENTS ###
 y = DF['Class'].to_numpy()
-lda = LDA(n_components=6)
+lda = LDA(n_components=4)
 X_lda = StandardScaler().fit_transform(lda.fit(X, y).transform(X))
 
-### TRAIN SVM CLASSIFIERS ON EACH X ###
+### FIND OPTIMAL SVM HYPERPARAMETERS FOR EACH REDUCED FEATURE SET ###
 from sklearn.svm import SVC
-'''$
+
 #grid search for optimal hyperparameters
 for X, feature_type in ((X_sisso, "SISSO"), (X_pca, "PCA"), (X_lda, "LDA")):
     parameters = {'kernel':('linear', 'rbf'), 'C':np.arange(1,10,1), 'gamma':['auto', 'scale']}
@@ -46,23 +45,18 @@ for X, feature_type in ((X_sisso, "SISSO"), (X_pca, "PCA"), (X_lda, "LDA")):
     temp = clf.cv_results_
     print("Best test score, " + feature_type + ":")
     print(np.amax(clf.cv_results_.get("mean_test_score")))
-        
-
+# OPTIMAL PARAMETERS FROM ABOVE CODE #
 # SISSO params @ idx 25: {'C': 7, 'gamma': 'auto', 'kernel': 'rbf'}
 # PCA params @ idx 0: {'C': 1, 'gamma': 'auto', 'kernel': 'linear'}
 # LDA params @ idx 4: {'C': 1, 'gamma': 'scale', 'kernel': 'rbf'}
-'''
 
-lda = LDA(n_components=6)
-X_lda = StandardScaler().fit_transform(lda.fit(X, y).transform(X))
-
+### TRAIN SVM CLASSIFIERS ON LDA FEATURE SET ###
 X_train, X_test, y_train, y_test = train_test_split(X_lda, y, test_size=0.30, random_state=6, stratify=y)
-
 svc = SVC(C=1, gamma='scale', kernel='rbf')
 svc.fit(X_train, y_train)
 y_pred = svc.predict(X_test)
 
-
-c = classification_report(y_test, y_pred, target_names=['Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5', 'Class 6', 'Class 7'])
-print(c)
+# print classification report
+target_names = ['Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5', 'Class 6', 'Class 7']
+print(classification_report(y_test, y_pred, target_names=target_names))
 
