58d182066c0077632da0bf265653218a208b5b81
Jacob Florian <florianj@Jacobs-MacBook-Pro.local>
2020-04-20 20:37:45 -0500

    random forest - include in zip file

--- forest.py
+++ forest.py
@@ -20,41 +20,49 @@ y = DF.iloc[:, 1]
 
 ################################# 30/70 Test/Train stratified split
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=20, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42, stratify=y)
 
 ################################## Generate LDA Features
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-lda = LDA(n_components=2)
+lda = LDA(n_components=5)
 X_train_lda = lda.fit_transform(X_train, y_train)
-X_test_lda = lda.fit_transform(X_test, y_test)
+X_test_lda = lda.transform(X_test)
 
 ################################## Generate PCA Features
 from sklearn.decomposition import PCA
-pca = PCA(n_components=2)
-X_train_pca = pca.fit_transform(X_train)
-X_test_pca = pca.fit_transform(X_test)
+pca = PCA(n_components=10)
+X_train_pca = pca.fit_transform(X_train, y_train)
+X_test_pca = pca.transform(X_test)
 
 XLDA = [X_train_lda, X_test_lda, y_train, y_test]
 XPCA = [X_train_pca, X_test_pca, y_train, y_test]
+XSTD = [X_train, X_test, y_train, y_test]
 
-X_train, X_test, y_train, y_test = train_test_split(X_sisso, y, test_size=0.30, random_state=20, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X_sisso, y, test_size=0.30, random_state=42, stratify=y)
 XSISSO = [X_train, X_test, y_train, y_test]
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
-for X, feature_type in ((XSISSO, "SISSO"), (XPCA, "PCA"), (XLDA, "LDA")):
-    parameters = {'min_samples_split':np.arange(2,3), 'max_depth':np.arange(2,10,2), 'max_leaf_nodes':np.arange(2,100,5)}
-    tree = RandomForestClassifier(n_estimators=200)
+
+for X, feature_type in (((XPCA, "PCA"), (XSISSO, "SISSO"), (XLDA, "LDA"), (XSTD, "XSTD"))):
+    parameters = {'min_samples_split':[2], 
+                 'max_depth':[5, 10, 20, None], 
+                 'max_leaf_nodes':[10, 40, 100], 
+                 'min_samples_leaf':[1, 2],
+                 'max_features':[1, 2, 'auto']}
+
+    tree = RandomForestClassifier(n_estimators=200, random_state=42)
 
     #Grid Search to find optimal parameters
     clf = GridSearchCV(tree, parameters, cv=5)
     clf.fit(X[0], X[2])
-	
+
     #Print Cross Validation Scores
-    #print("Best test score, " + feature_type + ":")
-    #print(np.amax(clf.cv_results_.get("mean_test_score")))
+    print(feature_type + "CV Scores: ", np.amax(clf.cv_results_.get('mean_test_score')))
 
     best_tree = clf.best_estimator_
+    print(clf.best_estimator_)
+
     best_tree.fit(X[0], X[2])
     y_pred = best_tree.predict(X[1])
     y_pred_train = best_tree.predict(X[0])
@@ -65,6 +73,7 @@ for X, feature_type in ((XSISSO, "SISSO"), (XPCA, "PCA"), (XLDA, "LDA")):
 
     print(feature_type + ' Train: ', accuracy_score(X[2], y_pred_train))
     print(feature_type + ' Test: ', accuracy_score(X[3], y_pred))
+    print(feature_type + ' Test: ', classification_report(X[3], y_pred))
 
 
 
